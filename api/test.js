5a3188aed5e0d8e9dc8290277e8013aac5621f13
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 19:49:51 -0400

    smaller code

--- api/test.js
+++ api/test.js
@@ -1,9 +1,8 @@
 const axios = require('axios').default;
 
 module.exports = async ( req, res ) => {
-  const order_id = req.query.order_id
-  const get_order_gql = `query MyQuery {
-    pickup_orders(where: {id: {_eq: ${order_id}}}) {
+  const get_order_gql = `query getCurrentOrder {
+    pickup_orders(where: {id: {_eq: ${req.query.order_id}}}) {
       customer_name
       order_items {
         notes
@@ -14,14 +13,11 @@ module.exports = async ( req, res ) => {
       }
     }
   }`
-
   const hasura_response = (
     await axios.post('https://hasura-3udj.onrender.com/v1/graphql', {
       query: get_order_gql
     })
   )
   const current_order = hasura_response.data.data
-  console.log(hasura_response)
-
   res.json({result: current_order})
 }
