bb4af04baeb9d9323cb02d7cb3d043a398502086
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 19:34:56 -0400

    hm

--- api/test.js
+++ api/test.js
@@ -1,50 +1,36 @@
 const axios = require('axios').default;
 module.exports = ( req, res ) => {
   const order_id = req.query.order_id
-  let response = 'nothing'
-  axios({
-    method: 'post',
-    headers: {'x-hasura-admin-secret': 'soupnazi'},
-    url: 'https://hasura-3udj.onrender.com/v1/graphql',
-    query: `query {
-              pickup_orders(where: {id: {_eq: ${order_id}}})
-            }`
-  }).catch(function (error) {
-    console.log('the error', error);
-  }).then(function (response) {
-    console.log('the data', response.data)
-    response = response.data
-    res.json({result: response})
-  });
+  // let response = 'nothing'
+  // axios({
+  //   method: 'post',
+  //   headers: {'x-hasura-admin-secret': 'soupnazi'},
+  //   url: 'https://hasura-3udj.onrender.com/v1/graphql',
+  //   query: `query {
+  //             pickup_orders(where: {id: {_eq: ${order_id}}})
+  //           }`
+  // }).catch(function (error) {
+  //   console.log('the error', error);
+  // }).then(function (response) {
+  //   console.log('the data', response.data)
+  //   response = response.data
+  // });
 
-
-
+  res.json({result: getOrder(order_id)})
 };
-//
-// import getOrders from '@/api/get_all_order'
-//
-// export default {
-//   // Data
-//   data() {
-//     return {
-//       orders: []
-//     }
-//   },
-//
-//   // Subscription / Real Time
-//   apollo: {
-//     $subscribe: {
-//       pickup_orders: {
-//         query: getOrders,
-//         variables() {
-//           return {
-//             ID: this.$route.params.id
-//           }
-//         },
-//         result({ data }) {
-//           this.orders = data.pickup_orders
-//         }
-//       }
-//     }
-//   }
-// }
+
+
+async function getOrder(order_id) {
+  try {
+    const response = (
+      await axios.post('https://hasura-3udj.onrender.com/v1/graphql', {
+        query:  `query {
+                  pickup_orders(where: {id: {_eq: ${order_id}}})
+                }`
+      })
+    ).data
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
