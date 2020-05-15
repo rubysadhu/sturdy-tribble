688b171d4bca8dd7d285e520900ce591fc88aa23
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 19:46:57 -0400

    go

--- api/test.js
+++ api/test.js
@@ -15,11 +15,13 @@ module.exports = async ( req, res ) => {
     }
   }`
 
-  const response = (
+  const hasura_response = (
     await axios.post('https://hasura-3udj.onrender.com/v1/graphql', {
       query: get_order_gql
     })
-  ).data
+  )
+  const current_order = hasura_response.data.data
+  console.log(hasura_response)
 
-  res.json({result: response})
+  res.json({result: current_order})
 }
