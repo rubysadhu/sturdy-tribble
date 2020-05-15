12251b15c540b4e23cab3b80ac5551ac15bf5532
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 17:37:32 -0400

    ..

--- api/send_sms.js
+++ api/send_sms.js
@@ -7,8 +7,6 @@ module.exports = ( req, res ) => {
   client.messages
     .create({
        body: `Does this work. Serverless Function.`,
-       statusCallback: 'https://postb.in/b/1589527924144-5734390488360',
-       mediaUrl: ['https://www.foodsafety.ca/sites/default/files/styles/mcl_img_featured_desk/public/2019-07/iStock-171589019.jpg?itok=Zfl3oEKL'],
        from: '+15878017440',
        to: '+16474068748'
      })
