910cdeec9236f6de51a292a5af2f3e9242662bfa
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 17:34:17 -0400

    .

--- api/send_sms.js
+++ api/send_sms.js
@@ -1,10 +1,9 @@
+const accountSid = 'ACfe59a083beab96197347e43a1c7652d6';
+const authToken = '1b7154c576de00d23b38f70e15643ab0';
+const client = require('twilio')(accountSid, authToken);
+
+
 module.exports = ( req, res ) => {
-  const accountSid = 'ACfe59a083beab96197347e43a1c7652d6';
-  const authToken = '1b7154c576de00d23b38f70e15643ab0';
-  const twilio = require('twilio');
-
-  const client = new twilio(accountSid, authToken);
-
   client.messages
     .create({
        body: `Does this work. Serverless Function.`,
@@ -14,7 +13,7 @@ module.exports = ( req, res ) => {
        to: '+16474068748'
      })
     .then(function(err, messages) {
-      console.log(err, message.sid)
+      console.log(err, messages.sid)
+      res.json( messages )
     });
-  console.log('hey')
 };
