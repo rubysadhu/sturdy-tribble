8469d990c1f443379794b526448fb01b58530e95
Wynter Jones <34955844+monetizedesign@users.noreply.github.com>
2020-05-15 20:13:31 -0400

    ..

--- api/sms_order_started.js
+++ api/sms_order_started.js
@@ -1,6 +1,7 @@
-module.exports = ( req, res ) => {
-  const axios = require('axios').default;
-  const twilioClient = require('twilio')('ACfe59a083beab96197347e43a1c7652d6', '3ff5e19d05443ac401e649c697e070de');
+const axios = require('axios').default;
+const twilioClient = require('twilio')('ACfe59a083beab96197347e43a1c7652d6', '3ff5e19d05443ac401e649c697e070de');
+
+module.exports = async ( req, res ) => {
   const order_id = req.query.order_id
   const get_order_gql = `query getCurrentOrder {
     pickup_orders(where: {id: {_eq: ${order_id}}}) {
